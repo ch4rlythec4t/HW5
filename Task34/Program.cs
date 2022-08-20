0573bd771af47e35aa1904c64a94fd6146db3337
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-20 11:26:49 +0300

    Task34 done

---
+++ Task34/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class Program
+{
+    static int[] CreateArray(int ArrayLength)
+    {
+        int[] CreateArray = new int[ArrayLength];
+        int CreateArrayCount = 0;
+        for (int i = 0; i < ArrayLength; i++)
+        {
+            CreateArray[i] = new Random().Next(100, 999);
+            if (CreateArray[i] % 2 == 0) CreateArrayCount++;
+        }
+
+        return CreateArray;
+    }
+
+    static int CountMultiplyOfTwoArray(int[] MultiplyOfTwoArray)
+    {
+
+        int CountMultiplyOfTwo = 0;
+        for (int i = 0; i < MultiplyOfTwoArray.Length; i++)
+        {
+            if ((MultiplyOfTwoArray[i] % 2) == 0) CountMultiplyOfTwo++;
+        }
+
+        return CountMultiplyOfTwo;
+    }
+    static void WriteArray(int[] WriteArray)
+    {
+        for (int i = 0; i < WriteArray.Length; i++)
+        {
+            if (i == 0) Console.Write("[" + WriteArray[i] + ", ");
+            else if (i == WriteArray.Length - 1) Console.Write(WriteArray[i] + "]");
+            else Console.Write(WriteArray[i] + ", ");
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите длину массива: ");
+
+        int N = Convert.ToInt32(Console.ReadLine());
+        int[] Array = CreateArray(N);
+        int Count = CountMultiplyOfTwoArray(MultiplyOfTwoArray: Array);
+
+        WriteArray(WriteArray: Array);
+        Console.Write(" -> " + Count);
+    }
+}
