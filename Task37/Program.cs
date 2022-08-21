7a327d3ea1314350466fb34a9b706bba50afa9b3
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-21 19:52:37 +0300

    CreateArray done

--- Task37/Program.cs
+++ Task37/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static int[] CreateArray()
+    {
+        Console.WriteLine("Введите длину массива: ");
+        newArrayLength = Convert.ToInt32.(Console.ReadLine());
+
+        int[] newArray = new int[newArrayLength];
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            newArray[i] = new Random().Next();
+        }
+
+        return newArray;
+    }
+
+    int[] MuliplicationPairArray(int[] Array)
+    {
+
+    }
+    static void Main()
+    {
+
+    }
+}
